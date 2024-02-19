c26b660f5b7669930f0e98064b296f1b5577e7bd
Insaram Alam <alaminsaram92@gmail.com>
2024-02-20 03:32:09 +0500

    done

--- src/utils/hooks/useDashboard.js
+++ src/utils/hooks/useDashboard.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useCreateRoomMutation } from "../../redux/apis/chatroom.api";
+import {
+  useCreateRoomMutation,
+  useGetAllChatRoomsQuery,
+  useGetMyChatRoomsQuery,
+  useJoinRoomMutation,
+} from "../../redux/apis/chatroom.api";
 import { useUser } from "./useUser";
 import { useNavigation } from "@react-navigation/native";
 
 export const useDashboard = () => {
   const navigation = useNavigation();
-  const dispatch = useDispatch();
   const {
     user: { _id: userId },
   } = useUser();
 
   const [createChatRoom] = useCreateRoomMutation();
+  const [joinChatRoom] = useJoinRoomMutation();
 
-  // create room
+  const { data: chatRooms = [],  } = useGetAllChatRoomsQuery();
+  const { data: chats = [], error,originalArgs } = useGetMyChatRoomsQuery({userId});
+
+  console.log(JSON.stringify(originalArgs, null, 2));
+
   const handleCreateRoom = async (name) => {
     try {
       const response = await createChatRoom({ author: userId, name });
@@ -35,5 +44,26 @@ export const useDashboard = () => {
     }
   };
 
-  return { handleCreateRoom };
+  const handleJoinRoom = async (roomId) => {
+    try {
+      const response = await joinChatRoom({ userId, roomId });
+      console.log("RESPONSE", JSON.stringify(response, null, 2));
+      if (response.error) {
+        return {
+          error:
+            response.error?.data?.message ||
+            response.error?.data?.error ||
+            response?.error?.error,
+        };
+      }
+      navigation.navigate("Inbox", {
+        chatId: response.data.chatRoom._id,
+        chatName: name,
+      });
+    } catch (error) {
+      console.log("ERROR CREATING ROOM====", error);
+    }
+  };
+
+  return { handleCreateRoom, chats, chatRooms, handleJoinRoom };
 };
